771923a101e8ad148f083a9349ebcf98edb0e791
Petelin Andrey <apetelin6821@gmail.ru>
2020-08-31 18:31:49 +0500

    Исправил работу саг

--- src/redux/sagas/rootSaga.js
+++ src/redux/sagas/rootSaga.js
@@ -1,10 +1,18 @@
 import { fork } from 'redux-saga/effects';
+import { REQUEST_POSTS, REQUEST_COMMENTS, REQUEST_USERS } from "../types";
 import sagaComments from './sagaComments';
 import sagaPosts from './sagaPosts';
 import sagaUsers from './sagaUsers';
 
-export default function* rootSaga() {
-    yield fork(sagaComments)
-    yield fork(sagaPosts)
-    yield fork(sagaUsers)
-  }
+export default function* rootSaga(data) {
+    switch(data.type) {
+        case REQUEST_POSTS:
+            return yield fork(sagaPosts);
+        case REQUEST_COMMENTS:
+            return yield fork(sagaComments, data.id);
+        case REQUEST_USERS:
+            return yield fork(sagaUsers);
+        default:
+            return
+    }    
+}
--- src/redux/sagas/sagaComments.js
+++ src/redux/sagas/sagaComments.js
@@ -2,9 +2,10 @@ import { put, call } from 'redux-saga/effects';
 import { FETCH_COMMENTS, } from "../types";
 import requestData from './requestData';
 
-export default function* (post) { 
+export default function* (id) { 
+    console.log(id);
     try {
-        const payload = yield call(requestData, `https://jsonplaceholder.typicode.com/comments?postId=${post.id}`);  
+        const payload = yield call(requestData, `https://jsonplaceholder.typicode.com/comments?postId=${id}`);  
         yield put({type: FETCH_COMMENTS, payload: payload.data})
     } 
     catch {
