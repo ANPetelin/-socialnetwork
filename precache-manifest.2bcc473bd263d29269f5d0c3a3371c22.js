0ce1c4addc433a914570ab7f779d8ef0588cf0a0
Petelin Andrey <apetelin6821@gmail.ru>
2020-08-31 14:02:39 +0500

    Updates

---
+++ precache-manifest.2bcc473bd263d29269f5d0c3a3371c22.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c702fb7640eb020ddaea5ce4bd7a0fd5",
+    "url": "/socialnetwork/index.html"
+  },
+  {
+    "revision": "af06d47c04651c552e54",
+    "url": "/socialnetwork/static/css/2.fb92a525.chunk.css"
+  },
+  {
+    "revision": "a36abe232684ab7e0cc6",
+    "url": "/socialnetwork/static/css/main.9ac1a355.chunk.css"
+  },
+  {
+    "revision": "af06d47c04651c552e54",
+    "url": "/socialnetwork/static/js/2.49619c72.chunk.js"
+  },
+  {
+    "revision": "e4f6fd5e687738d725e56603861f1923",
+    "url": "/socialnetwork/static/js/2.49619c72.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a36abe232684ab7e0cc6",
+    "url": "/socialnetwork/static/js/main.eee9a76d.chunk.js"
+  },
+  {
+    "revision": "2a3487472518acb5bff9",
+    "url": "/socialnetwork/static/js/runtime-main.c4be41fa.js"
+  }
+]);
